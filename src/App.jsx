1345003764153beb79ab414a13ae1ee07a420fca
KI-Anik <iamkhairul101@gmail.com>
2024-11-20 21:14:55 +0600

    fethed data and display in card

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Cards from "./components/Cards/Cards"
 import Footer from "./components/Footer/Footer"
 import Banner from "./components/Header/Banner"
 import Navbar from "./components/Header/Navbar"
 
 function App() {
-  const [] = useState([])
+  const [allPlayers, setAllPlayers] = useState([])
 
-  
+  useEffect(() =>{
+    fetch('../public/fake.json')
+    .then(res => res.json())
+    .then(data => setAllPlayers(data))
+  },[])
+   
   return (
     <>
       <div className="container mx-auto mt-4 space-y-12">
         <Navbar></Navbar>
         <Banner></Banner>
-        <Cards></Cards>
+        <Cards allPlayers={allPlayers}></Cards>
         <Footer></Footer>
       </div>
     </>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,15 +1,42 @@
 
-const Cards = () => {
+const Cards = ({ allPlayers }) => {
     return (
-        <div className="flex justify-between items-center">
-            <h2 className="text-2xl font-bold">Avaiable player</h2>
+        <div>
+            <div  className="flex justify-between items-center">
+            <h2 className="text-2xl font-bold">Avaiable player : {allPlayers.length}</h2>
             <div className="flex gap-4 border p-3 rounded-xl font-semibold">
-                <button>Available</button>
+                <button>Available </button>
                 <button>Selected</button>
             </div>
+            </div>
+            <div className="w-11/12 mx-auto grid grid-cols-3 gap-5 mt-8 ">
+
             {
-                
+                allPlayers.map(player =>
+                    <div key={player.playerId} className=" rounded-3xl bg-base-100 shadow-xl p-5">
+                        <figure className="">
+                            <img
+                            className='rounded-lg'
+                                src={player.image}
+                                alt="player image" />
+                        </figure>
+                        <div className="">
+                            <h2 className="card-title">{player.name}</h2>
+                            <p>{player.country}</p>
+                            <p>{player.role }</p>
+                            <hr />
+                            <h4 className="font-bold">Rating</h4>
+                            <p>{player.battingType}</p>
+                            <div className="flex justify-between">
+                                <h4>Price : {player.biddingPrice}</h4>
+                                <button className=" bg-gray-400">choose Player</button>
+                            </div>
+                        </div>
+                    </div>
+                )
             }
+            </div>
+
         </div>
     );
 };
